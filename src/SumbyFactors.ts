7249744cd2ea673b987c775f7cf5b3bbe49fd96d
ealonsol <ealonsol@hotmail.com>
2019-04-30 17:08:38 -0500

    kata 30/04/2019

--- src/SumbyFactors.ts
+++ src/SumbyFactors.ts
@@ -1,18 +1,56 @@
 export function sumOfDivided(lst: number[]): number[][] {
-    let result: number[] = [];
-    let resultAux: number[] = [];
-    result = factoresPrimos(lst[0]);
+    let factoresPrimosArreglo: number[] = [];
+    let factoresPrimosArregloAux: number[] = [];
+    let sumaFactoresPrimosArreglo: number[] = [];
+    factoresPrimosArreglo = factoresPrimos(lst[0]);
+
     for (let i: number = 1; i < lst.length; i++) {
-        resultAux = factoresPrimos(i);
+        factoresPrimosArregloAux = factoresPrimos(lst[i]);
+        for (let index: number = 0; index < factoresPrimosArregloAux.length;  index++ ) {
+            if (encontrarNumeroArregloDevolverlo(factoresPrimosArregloAux[index],
+                factoresPrimosArreglo) === -1 ) {
+                factoresPrimosArreglo.push(factoresPrimosArregloAux[index]);
+            }
+        }
     }
+    factoresPrimosArreglo = factoresPrimosArreglo.sort();
+    for (let index = 0; index < factoresPrimosArreglo.length; index++) {
+        let auxiliar: number = factoresPrimosArreglo[index];
+        sumaFactoresPrimosArreglo.push([ auxiliar, sumaValoresFactoresArreglo(lst, factoresPrimosArreglo[index]) ]);
+        console.log(factoresPrimosArreglo[index]);
+    }
+
+    return sumaFactoresPrimosArreglo;
 }
 
+export function sumaValoresFactoresArreglo(numeroArreglo: number[], divisor: number): number {
+    let suma: number = 0;
+    for (let index: number = 0; index < numeroArreglo.length; index++ ) {
+         if ( esNumeroDivisor(numeroArreglo[index], divisor )) {
+             suma += numeroArreglo[index];
+         }
+    }
+    return suma;
+}
 
+export function encontrarNumeroArreglo(numero: number, arreglo: number[]): boolean {
+    const encontrado = arreglo.find((element) => {
+        return element === numero ;
+      });
+    return encontrado ? true : false;
+}
+
+export function encontrarNumeroArregloDevolverlo(numero: number, arreglo: number[]): number {
+    const encontrado = arreglo.find((element) => {
+        return element === numero ;
+      });
+      return encontrado ? encontrado : -1;
+}
 
 export function factoresPrimos(numero: number): number[] {
-    let result: number[] = [];
+    const result: number[] = [];
     for (let i: number = 2; i <= numero; i++) {
-        if ( (esPrimo(i)) && (numero % i === 0)) {
+        if ( (esPrimo(i)) && (esNumeroDivisor(numero, i))) {
             result.push(i);
         }
     }
@@ -21,7 +59,7 @@ export function factoresPrimos(numero: number): number[] {
 export function esPrimo(numero: number): boolean {
     let divisores: number = 0;
     for (let i: number = 1; i <= numero; i++) {
-        if (numero % i === 0) {
+        if (esNumeroDivisor(numero, i)) {
             divisores++;
         }
     }
@@ -29,3 +67,11 @@ export function esPrimo(numero: number): boolean {
         return true;
     } else { return false; }
 }
+
+export function esNumeroDivisor(dividendo: number, divisor: number ): boolean {
+    return dividendo % divisor === 0 ? true : false;
+}
+
+(() => {
+    console.log(sumOfDivided([15, 21, 24, 30, 45]));
+})();
--- tests/SumbyFactors.spec.ts
+++ tests/SumbyFactors.spec.ts
@@ -1,9 +1,18 @@
-import { esPrimo, factoresPrimos, sumOfDivided } from "../src/SumbyFactors";
+import { encontrarNumeroArreglo, encontrarNumeroArregloDevolverlo,
+    esPrimo, factoresPrimos, sumaValoresFactoresArreglo,
+     sumOfDivided } from "../src/SumbyFactors";
 describe("haciendo pruebas al algoritmo de factores primos", () => {
-    const esperado = [ [2, 12], [3, 27], [5, 15] ];
+    //const esperado = [ [2, 12], [3, 27], [5, 15] ];
     const factores = [ 3, 5 ];
+
+    test("Probando con el arreglo [15,21,24,30,45] ", () => {
+        expect(sumOfDivided([15, 21, 24, 30, 45])).
+            toEqual(expect.
+                arrayContaining( [ [2, 3, 5, 7] ] ));
+    });
+
     test("Probando con el arreglo [12, 15] ", () => {
-        expect(sumOfDivided([12, 15])).toEqual(expect.arrayContaining(esperado));
+        expect(sumOfDivided([12, 15])).toEqual(expect.arrayContaining([ [2, 12], [3, 27], [5, 15] ]));
     });
 
     test("Probando numero primo 2 ", () => {
@@ -27,5 +36,30 @@ describe("haciendo pruebas al algoritmo de factores primos", () => {
     });
     test("devolviendo factores primos ", () => {
         expect(factoresPrimos(21)).toEqual(expect.arrayContaining([3, 7]));
+    });
+    test("buscando numero en arreglo ", () => {
+        expect(encontrarNumeroArreglo(60, [29, 30, 45, 58, 60])).toBe(true);
+    });
+    test("buscando numero en arreglo ", () => {
+        expect(encontrarNumeroArreglo(1, [29, 30, 45, 58, 60])).toBe(false);
+    });
+
+    test("encontrando numero en arreglo ", () => {
+        expect(encontrarNumeroArregloDevolverlo(29, [29, 30, 45, 58, 60])).toBe(29);
+    });
+    test("encontrando numero en arreglo ", () => {
+        expect(encontrarNumeroArregloDevolverlo(1, [29, 30, 45, 58, 60])).toBe(-1);
+    });
+    test("suma todos los numeros que son divisores ", () => {
+        expect(sumaValoresFactoresArreglo([29, 30, 45, 58, 60], 2 )).toBe(148);
+    });
+    test("suma todos los numeros que son divisores ", () => {
+        expect(sumaValoresFactoresArreglo([29, 30, 45, 58, 60], 3 )).toBe(135);
+    });
+    test("suma todos los numeros que son divisores ", () => {
+        expect(sumaValoresFactoresArreglo([29, 30, 45, 58, 60], 2 )).toBe(140);
+    });
+    test("suma todos los numeros que son divisores ", () => {
+        expect(sumaValoresFactoresArreglo([15, 21, 24, 30, 45], 7 )).toBe(21);
     });
 });
